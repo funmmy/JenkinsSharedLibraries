abe8b91be39ac27bcb2d3f0de3e8a87067fdb9a9
igbasanmi <105608298+igbasanmi@users.noreply.github.com>
2022-07-04 20:31:30 -0400

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,13 +1,13 @@
 def call(String, stageName){
-  if ("${stageName}" == "Build")
+  if (${stageName} == "Build")
 	{
 	sh "mvn clean package" 
 	}
-  else if ("${stageName}" == "SonarQube Report")
+  else if (${stageName} == "SonarQube Report")
   	{
 	sh "mvn clean sonar:sonar"
 	}
-  else if ("${stageName}" == "Upload Into Nexus")
+  else if (${stageName} == "Upload Into Nexus")
   	{
 	sh "mvn clean deploy"
 	}
